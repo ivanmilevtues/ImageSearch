dcf360eee5dc12105a829cab1162cac91c0b1832
ivanmilevtues <ivanmilevtues@gmail.com>
2018-06-06 18:38:28 +0300

    Added image-listing component

--- ImageSearch/src/app/app.module.ts
+++ ImageSearch/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ImageListingComponent } from './image-listing/image-listing.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListingComponent
   ],
   imports: [
     BrowserModule
---
+++ ImageSearch/src/app/image-listing/image-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-listing',
+  templateUrl: './image-listing.component.html',
+  styleUrls: ['./image-listing.component.css']
+})
+export class ImageListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
